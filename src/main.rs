d1baed8e37bf8044041dfbcc614c2ac2210d7950
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-09-22 15:52:14 +0300

    Learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    println!("Enter the guess");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read the line");
+
+    println!("You guessed: {}", guess);
 }
